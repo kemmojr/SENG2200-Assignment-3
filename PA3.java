cd024e899643b9ac2e36b6ec7b3f570e48f02e36
Tim Kemmis <kemmojr@gmail.com>
2020-05-26 12:29:56 +1000

    Added the abstract class interStageStorage

--- PA3.java
+++ PA3.java
@@ -1,2 +1,13 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
 public class PA3 {
+    public static void main(int args[]){
+        int M, N, Qmax;
+        M = args[0];
+        N = args[1];
+        Qmax = args[2];
+
+    }
 }
---
+++ interStageStorage.java
@@ -0,0 +1,6 @@
+public abstract class interStageStorage {
+    double averageTime, averageNumOfItems;
+
+    public abstract double averageTime();
+    public abstract double averageNumOfItems();
+}
